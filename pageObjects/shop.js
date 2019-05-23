f0101de031c8b062c9d86fef70e3509ea185e53d
dabbmg <dabb.mg@gmail.com>
2019-05-23 12:18:38 -0600

    Fleshing out

--- pageObjects/shop.js
+++ pageObjects/shop.js
@@ -3,9 +3,28 @@ let footers = require('../test-assets/footers')
 let genBrowse = require('../test-assets/genBrowse')
 let headerFilter = require('../test-assets/headerFilter')
 let searchBar = require('../test-assets/searchBar')
-
+//
 var shopCommands = {
-    
+    accFeat: function(shopPage) {
+        accFeatures(shopPage)
+        return this
+    },
+    foot: function(shopPage) {
+        footers(shopPage)
+        return this
+    },
+    browse: function(shopPage) {
+        genBrowse(shopPage)
+        return this
+    },
+    headFilt: function(shopPage) {
+        headerFilter(shopPage)
+        return this
+    },
+    search: function(shopPage) {
+        searchBar(shopPage)
+        return this
+    }  
 }
 module.exports = {
     url: 'https://shop.tesla.com',
